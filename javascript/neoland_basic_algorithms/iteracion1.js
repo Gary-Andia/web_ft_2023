07a7dc89924b4f57c09b669231289d327bca89be
Gary-Andia <ogaryinfo@gmail.com>
2023-01-10 22:41:39 +0100

    iteracion1

--- javascript/neoland_basic_algorithms/iteracion1.js
+++ javascript/neoland_basic_algorithms/iteracion1.js
@@ -5,5 +5,6 @@ let x = 50;
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 let h = 5;
 let y = 10;
-console.log(h);
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+let z = h + y;
+console.log(z);
