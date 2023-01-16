cfb598690e5baac4ebdfefc8265608951fb3b6f8
Gary-Andia <ogaryinfo@gmail.com>
2023-01-16 17:27:17 +0100

    funciones done

--- javascript/JS_Functions/iteracion6.js
+++ javascript/JS_Functions/iteracion6.js
@@ -1,17 +1,27 @@
 // 1- Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
 const duplicates = [
-    'sushi',
-    'pizza',
-    'burger',
-    'potatoe',
-    'pasta',
-    'ice-cream',
-    'pizza',
-    'chicken',
-    'onion rings',
-    'pasta',
-    'soda'
-  ];
-  function removeDuplicates(param) {
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
 
-  }
+function removeDuplicates(param) {
+  const same = [];
+
+  duplicates.forEach((word) => {
+    if (!same.includes(word)) {
+      same.push(word);
+    }
+  });
+  console.log(same);
+}
+
+removeDuplicates(duplicates);
